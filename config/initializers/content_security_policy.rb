8b8345f1543c195300ddd0fa8686802b4ddd9e92
ximidas <ximidas@ximidas.com>
2019-12-01 17:46:14 +0200

    JS bug fix production

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -35,6 +35,6 @@ Rails.application.config.content_security_policy do |policy|
     policy.script_src :self, :https, :unsafe_inline
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
   else
-    policy.script_src :self, :https
+    policy.script_src :self, :https, :unsafe_inline
   end
 end
